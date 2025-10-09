d71183f9645f280b1671d8fbdff7970e120afe6d
AndreyKuchuk518 <andrey.kuchuk.work@gmail.com>
2025-10-09 20:32:00 +0300

    Update task6.py

--- lab2/task6.py
+++ lab2/task6.py
@@ -1,1 +1,17 @@
+info = {
+    "a": 5,
+    "b": 15,
+    "c": 8,
+    "d": 20,
+    "e": 3
+}
 
+filtered_info = {}
+
+for key, value in info.items():   # перебор все пары ключ-значение, items возвращает ключ и значение отновременно
+    
+    if value >= 10:
+        filtered_info[key] = value # добавляем пару в словарь
+
+print("Исходный словарь:", info)
+print("Словарь отфильтрованный:", filtered_info)

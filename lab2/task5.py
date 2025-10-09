87facb513363cbb3cef17210aeaff014ad5dc574
AndreyKuchuk518 <andrey.kuchuk.work@gmail.com>
2025-10-09 20:21:31 +0300

    Update task5.py

--- lab2/task5.py
+++ lab2/task5.py
@@ -1,1 +1,10 @@
+strings = ("яблоко", "банан", "ананас", "мандарин", "арбуз")
+longest = strings[0]  # пусть первый элемент самый длинный
 
+for s in strings: # перебор элементов
+    if len(s) > len(longest): # len - определяет длину строки
+        longest = s
+
+print("Кортеж строк:", strings)
+print("Самая длинная строка:", longest)
+print("Её длина:", len(longest))

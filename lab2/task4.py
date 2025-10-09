598876cc888b37dabb56d5797abb028dc155a3bd
AndreyKuchuk518 <andrey.kuchuk.work@gmail.com>
2025-10-09 20:16:08 +0300

    Update task4.py

--- lab2/task4.py
+++ lab2/task4.py
@@ -1,1 +1,12 @@
+input_numbers = input("Введите числа через пробел: ")
+numbers = list(map(int, input_numbers.split()))
 
+total = 0
+for num in numbers:
+    total = total + num  #сумма
+
+count = len(numbers)  # для подсчёта элементов
+average = total / count
+
+print("Список:", numbers)
+print("Среднее арифметическое:", average)

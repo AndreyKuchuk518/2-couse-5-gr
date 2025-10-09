066bbce833b073839065a5c0490fdda21d7163a8
AndreyKuchuk518 <andrey.kuchuk.work@gmail.com>
2025-10-09 20:48:04 +0300

    Update task8.py

--- lab2/task8.py
+++ lab2/task8.py
@@ -1,1 +1,10 @@
+numbers = [5, 10, 12, 15, 20, 23, 25, 30]
 
+filtered_numbers = [] # список с не делящимися на 5 числами
+
+for num in numbers:
+    if num % 5 != 0:
+        filtered_numbers.append(num)
+
+print("Исходный список:", numbers)
+print("Список без чисел, делящихся на 5:", filtered_numbers)

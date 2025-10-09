6a6ddf2bf14265631ad0419d906ea43719670722
AndreyKuchuk518 <andrey.kuchuk.work@gmail.com>
2025-10-09 19:58:44 +0300

    Update task2.py

--- lab2/task2.py
+++ lab2/task2.py
@@ -1,1 +1,10 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+odd_numbers = []
 
+for num in numbers:
+    if num % 2 != 0:  
+        odd_numbers.append(num)  
+
+
+print("Исходный список:", numbers)
+print("Список нечётных чисел:", odd_numbers)

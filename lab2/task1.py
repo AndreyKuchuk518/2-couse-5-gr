fcf6df443cee9b3aa3c34cc51515193cce24f310
AndreyKuchuk518 <andrey.kuchuk.work@gmail.com>
2025-10-09 19:54:18 +0300

    Update task1.py

--- lab2/task1.py
+++ lab2/task1.py
@@ -1,1 +1,8 @@
-print 
+n = input("Введите число N: ")  
+product = 1  
+
+for digit_char in n:
+    digit_int = int(digit_char)
+    product = product * digit_int
+
+print("Произведение всех цифр числа:", product)

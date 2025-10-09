4ae306ebd785f9a65360caac93c189619d9993e6
AndreyKuchuk518 <andrey.kuchuk.work@gmail.com>
2025-10-09 20:05:28 +0300

    Update task3.py

--- lab2/task3.py
+++ lab2/task3.py
@@ -1,1 +1,11 @@
+input1 = input("Введите элементы первого множества через пробел: ")
+set1 = set(map(int, input1.split()))
 
+input2 = input("Введите элементы второго множества через пробел: ")
+set2 = set(map(int, input2.split()))
+
+only_in_one = set1.symmetric_difference(set2)
+
+print("Множество 1:", set1)
+print("Множество 2:", set2)
+print("Элементы, которые есть только в одном из множеств:", only_in_one)

d215ec7418f8756d7647de1b743470d1a02942ec
AndreyKuchuk518 <andrey.kuchuk.work@gmail.com>
2025-10-09 20:39:17 +0300

    Update task7.py

--- lab2/task7.py
+++ lab2/task7.py
@@ -1,1 +1,11 @@
+text = input("Введите строку: ")
+vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯ"  # Определяем гласные буквы 
 
+consonant_count = 0 # счётчик согласных
+
+for char in text: # перебор каждого символа
+    if char.isalpha():  # isalpha() — проверяет, буква ли это
+        if char not in vowels:  # если не гласная — значит, согласная
+            consonant_count += 1
+
+print("Количество согласных букв:", consonant_count)
